4e1576b02a938a7bb0c1d7ad6b695a35a4cfecbe
James Bubb <jpbubb82@gmail.com>
2021-06-30 12:54:01 +0100

    feat: create setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Cats from "./features/CatList";
-import Header from "./Header";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Cats />
     </div>
   );
 }
--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  catsSlice from './features/catsSlice';
-
-export const store = configureStore({
-  reducer: catsSlice,
-});
-
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
